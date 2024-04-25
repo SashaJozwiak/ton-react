af6c7c06859085d30fd8c89117b02c16b8d93bc0
SashaJozwiak <wmdz88@gmail.com>
2024-04-25 23:26:43 +0300

    feat: init some data and design

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,6 +3,10 @@ import { AuthData } from '../../App';
 import { getActivities } from '../../utils/queries/fetchData';
 import { ActivityData } from '../../utils/queries/fetchData';
 import { refreshAccessToken } from '../../utils/queries/refreshAccessToken';
+import { lvls } from '../../utils/math/lvls';
+
+import './Main.css';
+import { calculateLvl, sumPointsFn } from '../../utils/math/points';
 
 interface MainProps {
     userId: number;
@@ -10,11 +14,27 @@ interface MainProps {
     authData: AuthData | null;
 }
 
+export interface IProgress {
+    current_lvl: number,
+    current_points: number,
+    start_lvl: number,
+    next_lvl: number,
+}
+
+
+
 export const Main: React.FC<MainProps> = ({ userId, setUserId, authData }) => {
     const [activData, setActivData] = useState<ActivityData>({
         steps: 0,
         cardio: 0,
         calories: 0,
+    });
+    const [sumPoints, setSumPoints] = useState<number>(0);
+    const [progress, setProgress] = useState<IProgress>({
+        current_lvl: 0,
+        current_points: 0,
+        start_lvl: 0,
+        next_lvl: 0,
     });
 
     async function fetchDataFromGoogleFit(token: string) {
@@ -46,25 +66,64 @@ export const Main: React.FC<MainProps> = ({ userId, setUserId, authData }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    useEffect(() => {
+        sumPointsFn(activData, setSumPoints);
+
+    }, [activData])
+
+    useEffect(() => {
+        const differenceMilliseconds = new Date().getTime() - new Date('2024-04-01').getTime();
+        const passedDays = Math.floor(differenceMilliseconds / 86400000);
+        const pointsPerDay = sumPoints / passedDays;
+        calculateLvl(lvls, pointsPerDay, setProgress)
+    }, [sumPoints])
+
+
+    console.log(progress)
     return (
-        <div>
-            <h1>Demo season</h1>
-            <p style={{ marginBottom: '1em' }}>from April 1, 2024</p>
-            <p>main_userId: {userId}</p>
+        <div style={{ fontFamily: 'monospace' }}>
+            <h1 style={{ paddingTop: '1rem' }}>Demo season</h1>
+            <p style={{ marginBottom: '2rem' }}>from April 1, 2024</p>
+            {/* <p>main_userId: {userId}</p> */}
             {!userId && <p>не получил userId</p>}
 
-            {authData && <pre>bdId:{authData.id}</pre>}
+            {/* {authData && <pre>bdId:{authData.id}</pre>} */}
 
-            <div style={{ border: '1px solid grey', padding: '1em', width: '50' }}>
-                <h1>Points: {((activData.steps + activData.calories + (activData.cardio * 100)) / 1000).toFixed(3) || 0}</h1>
+
+            <div style={{ margin: '2rem 2rem 0.5rem 2rem', width: '50' }}>
+                <h1 style={{ fontSize: '3rem', color: 'rgb(14 165 233)', fontFamily: 'monospace' }}>{sumPoints}</h1>
+            </div>
+            <div style={{ marginBottom: '2rem' }}>
+
+                <div style={{ margin: '5px 1em 5px' }} className='progress'>
+                    <div style={{ width: `${(progress.current_points - progress.start_lvl) / (progress.next_lvl - progress.start_lvl) * 100}%` }} className="progress-bar"></div>
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', margin: '0 1rem' }}>
+                    <div>
+                        <p>{progress.start_lvl}</p>
+                        <p>per day</p>
+                    </div>
+
+                    <h2 style={{ fontFamily: 'monospace' }}>Level {progress.current_lvl}</h2>
+
+                    <div>
+                        <p>{progress.next_lvl}</p>
+                        <p>per day</p>
+                    </div>
+
+                </div>
             </div>
 
-            <div style={{ float: "left", width: '50%' }}>
-                <h3 style={{ textAlign: 'left' }}>Cardio: {activData.cardio && activData.cardio}</h3>
-                <h3 style={{ textAlign: 'left' }}>Kcal: {activData.calories && activData.calories}</h3>
-                <h3 style={{ textAlign: 'left' }}>Steps: {activData.steps && activData.steps}</h3>
+            <div style={{ float: "left", width: '50%', margin: '5px 1em 5px' }}>
+                <div>
+                    <h3 style={{ textAlign: 'left' }}>Cardio: {activData.cardio && activData.cardio}</h3>
+                    <h3 style={{ textAlign: 'left' }}>Kcal: {activData.calories && activData.calories}</h3>
+                    <h3 style={{ textAlign: 'left' }}>Steps: {activData.steps && activData.steps}</h3>
+                </div>
+                <p>====</p>
             </div>
-            <p>====</p>
+
 
         </div>
     )
---
+++ src/utils/math/points.ts
@@ -0,0 +1,25 @@
+import { ActivityData } from '../queries/fetchData';
+//import { IProgress } from '../../components/main/Main';
+
+export function sumPointsFn(activData: ActivityData, setSumPoints: (sumPoints: number) => void) {
+    const sum = +(((activData.steps + activData.calories + (activData.cardio * 1000)) / 1000).toFixed(3)) || 0.000;
+    setSumPoints(sum)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function calculateLvl(data: any, value: number, setProgress: any) {
+    for (let i = 0; i < data.length; i++) {
+        if (
+            value >= data[i].need &&
+            (i === data.length - 1 || value < data[i + 1].need)
+        ) {
+            setProgress({
+                current_lvl: data[i].lvl,
+                current_points: value,
+                start_lvl: data[i].need,
+                next_lvl: data[i + 1] ? data[i + 1].need : null,
+            });
+        }
+    }
+    return null;
+}
