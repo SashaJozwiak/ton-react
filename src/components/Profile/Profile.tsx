291fda9b98f2a70971f0637aa82ff22998958fc0
SashaJozwiak <wmdz88@gmail.com>
2024-05-16 16:36:56 +0300

    feat: profile sample data test7

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,9 +2,16 @@ import WebApp from "@twa-dev/sdk";
 import { BackButton } from "@twa-dev/sdk/react";
 import { useEffect, useState } from "react";
 
+import loader from '../../assets/loading-gif.gif'
+
+
+interface UserData {
+    username: string | undefined;
+}
+
 const Profile = ({ userId, setRoutes }) => {
 
-    const [userAppData, setUserAppData] = useState({});
+    const [userAppData, setUserAppData] = useState<UserData>({ username: undefined });
 
     console.log(userId)
     console.log(WebApp.initDataUnsafe)
@@ -18,7 +25,7 @@ const Profile = ({ userId, setRoutes }) => {
             try {
                 if (WebApp.initDataUnsafe.user) {
                     const getUserData = WebApp.initDataUnsafe.user;
-                    setUserAppData(getUserData || {});
+                    setUserAppData({ username: getUserData.username });
                 } else {
                     console.log('not init');
 
@@ -36,7 +43,7 @@ const Profile = ({ userId, setRoutes }) => {
     return (
         <div>
             <BackButton onClick={() => setRoutes('main')} />
-            <h1>Profile</h1>
+            <h1>Hi, {userAppData?.username || <img src={loader} alt="loader" width='5%'></img>}</h1>
         </div>
     )
 }
