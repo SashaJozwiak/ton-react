a8361db9ea6cf702b176c6577cf6f799ee776a75
SashaJozwiak <wmdz88@gmail.com>
2024-05-16 17:01:59 +0300

    feat: add ton connect button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,9 @@ import WebApp from "@twa-dev/sdk";
 import { BackButton } from "@twa-dev/sdk/react";
 import { useEffect, useState } from "react";
 
+import { TonConnectButton } from '@tonconnect/ui-react';
+//import { useTonConnect } from './hooks/useTonConnect';
+
 import loader from '../../assets/loading-gif.gif'
 
 
@@ -10,17 +13,12 @@ interface UserData {
 }
 
 const Profile = ({ userId, setRoutes }) => {
-
     const [userAppData, setUserAppData] = useState<UserData>({ username: undefined });
 
-    console.log(userId)
-    console.log(WebApp.initDataUnsafe)
-    console.log(WebApp.initDataUnsafe.user)
-
+    console.log('userId', userId);
     console.log('userAppData', userAppData);
 
     useEffect(() => {
-
         const fetchData = async () => {
             try {
                 if (WebApp.initDataUnsafe.user) {
@@ -28,22 +26,20 @@ const Profile = ({ userId, setRoutes }) => {
                     setUserAppData({ username: getUserData.username });
                 } else {
                     console.log('not init');
-
                 }
-                //await fetchUserData();
             } catch (error) {
                 console.error('Error fetching user data:', error);
             }
         };
-
         fetchData();
-
     }, [])
 
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
             <BackButton onClick={() => setRoutes('main')} />
-            <h1>Hi, {userAppData?.username || <img src={loader} alt="loader" width='5%'></img>}</h1>
+            <h2>Hi, {userAppData?.username || <img src={loader} alt="loader" width='5%'></img>}</h2>
+
+            <TonConnectButton />
         </div>
     )
 }
