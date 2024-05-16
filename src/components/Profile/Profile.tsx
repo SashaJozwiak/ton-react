cd13018d34c61ed7fe3cd82966745a1d46cd821c
SashaJozwiak <wmdz88@gmail.com>
2024-05-16 11:36:17 +0300

    feat: profile sample data test2

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,38 @@
 import WebApp from "@twa-dev/sdk";
+import { useEffect, useState } from "react";
 
 
 const Profile = ({ userId }) => {
 
+    const [userAppData, setUserAppData] = useState({});
+
     console.log(userId)
     console.log(WebApp.initDataUnsafe)
     console.log(WebApp.initDataUnsafe.user)
+
+
+    console.log('userAppData', userAppData);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                if (WebApp.initDataUnsafe.user) {
+                    const getUserData = WebApp.initDataUnsafe.user;
+                    setUserAppData(getUserData || {});
+                } else {
+                    console.log('not init');
+
+                }
+                //await fetchUserData();
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchData();
+
+    }, [])
 
     return (
         <div>
