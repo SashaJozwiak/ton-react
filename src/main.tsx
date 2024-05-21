f5af7915764eb5a57579e6a946f6811907fa54e2
SashaJozwiak <wmdz88@gmail.com>
2024-05-21 13:33:50 +0300

    fix: some lex

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx';
 
-
-
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import WebApp from '@twa-dev/sdk'
 
 import './index.css'
-
-import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
 
 const manifestUrl = 'https://sashajozwiak.github.io/ton-react/tonconnect-manifest.json';
-console.log('manifestUrl', manifestUrl)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
