741eac49c7058aeb5af8f6a052c31931fe857e4a
SashaJozwiak <wmdz88@gmail.com>
2024-05-31 03:13:51 +0300

    fix: get userData test2

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -9,12 +9,14 @@ import loader from '../../assets/loading-gif.gif'
 
 interface UserData {
     username: string | undefined;
+    first_name: string | undefined;
+    last_name: string | undefined;
 }
 
 const Profile = ({ userId, setRoutes }) => {
     //const { connected } = useTonConnect();
 
-    const [userAppData, setUserAppData] = useState<UserData>({ username: undefined });
+    const [userAppData, setUserAppData] = useState<UserData>({ username: undefined, first_name: undefined, last_name: undefined });
 
     const [link, setLink] = useState<string>('https://t.me/zwiak');
     const [copied, setCopied] = useState<boolean>(false);
@@ -38,7 +40,7 @@ const Profile = ({ userId, setRoutes }) => {
                 if (WebApp.initDataUnsafe.user) {
                     const getUserData = WebApp.initDataUnsafe.user;
                     console.log('userData', getUserData);
-                    setUserAppData({ username: getUserData.username });
+                    setUserAppData({ username: getUserData.username, first_name: getUserData.first_name, last_name: getUserData.last_name });
                 } else {
                     console.log('not init');
                 }
@@ -52,7 +54,7 @@ const Profile = ({ userId, setRoutes }) => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
             <BackButton onClick={() => setRoutes('main')} />
-            <h2>Hi, {userAppData?.username || <img src={loader} alt="loader" width='5%'></img>}</h2>
+            <h2>Hi, {userAppData?.first_name || userAppData?.last_name || userAppData?.username || <img src={loader} alt="loader" width='5%'></img>}</h2>
 
             <TonConnectButton />
             <div style={{ marginTop: '1em', display: 'flex', flexDirection: 'column' }}>
